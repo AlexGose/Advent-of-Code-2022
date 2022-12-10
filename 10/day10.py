3992824dbdf49d3b4aad784c4b0a7378c151655f
Alex Gose <alexgose@users.noreply.github.com>
2022-12-10 08:08:44 -0500

    Upload solution for day 10 part 2

--- 10/day10.py
+++ 10/day10.py
@@ -2,6 +2,14 @@
 
 # Advent of code 2022, day 10
 
+
+def print_screen(crt):
+    for row in crt:
+        for col in row:
+            print(col, end='')
+        print()
+
+
 if __name__ == '__main__':
     lines = open(0).read().rstrip('\n').split('\n')
 
@@ -11,6 +19,8 @@ if __name__ == '__main__':
     cycle = 0
     p1=0
     cycles_left = 0
+    CRT = [['X' for col in range(40)] for row in range(6)]
+    row = 0
     for line in lines:
         if line[0:4] == 'noop':
             cycles_left = 1
@@ -24,14 +34,22 @@ if __name__ == '__main__':
             signal_strength = cycle * X
             if (cycle - 20) % 40 == 0:
                 p1 += signal_strength
-                print(f'cycle={cycle}, signal_strength={signal_strength}')
+                #print(f'cycle={cycle}, signal_strength={signal_strength}')
+            row = (cycle-1) // 40 % 6
+            col = (cycle-1) % 40
+            #print('row=',row,'col=',col,'X=',X)
+            if X-1 <= col <= X+1:
+                CRT[row][col] = '#'
+                #print(f'CRT[row][col]={CRT[row][col]}')
+            else:
+                CRT[row][col] = '.'
+            #print_screen(CRT)
             cycles_left -= 1
-        X += addx
+            if cycles_left == 0:
+                X += addx
 
     print('Part 1:')
     print(f'p1={p1}')
-    
-    p2=0
-    print()
+
     print('Part 2:')
-    print(f'p2={p2}')
+    print_screen(CRT)
